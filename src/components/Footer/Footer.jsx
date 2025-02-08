73677d415a33ffdc29c387f0fcb6a9b7321dc3f6
Anzhelika <anzhelikasilkina@yandex.ru>
2025-02-08 23:22:38 +0300

    rewrite footer with hooks

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,23 @@
-import { Component } from 'react';
 import './Footer.css';
 import PropTypes from 'prop-types';
 
 import TasksFilter from './TasksFilter';
 
-export default class Footer extends Component {
-  static propTypes = {
-    left: PropTypes.number,
-    selectTasks: PropTypes.func,
-    filter: PropTypes.string,
-    onClearCompleted: PropTypes.func,
-  };
-
-  static defaultProps = {
-    left: 0,
-    filter: 'all',
-  };
-
-  render() {
-    const { left, selectTasks, filter, onClearCompleted } = this.props;
-    return (
-      <footer className="footer">
-        <span className="todo-count">{left} items left</span>
-        <TasksFilter selectTasks={selectTasks} filter={filter} />
-        <button className="clear-completed" onClick={onClearCompleted}>
-          Clear completed
-        </button>
-      </footer>
-    );
-  }
+export default function Footer({ left, selectTasks, filter, onClearCompleted }) {
+  return (
+    <footer className="footer">
+      <span className="todo-count">{left} items left</span>
+      <TasksFilter selectTasks={selectTasks} filter={filter} />
+      <button className="clear-completed" onClick={onClearCompleted}>
+        Clear completed
+      </button>
+    </footer>
+  );
 }
+
+Footer.propTypes = {
+  left: PropTypes.number,
+  selectTasks: PropTypes.func,
+  filter: PropTypes.string,
+  onClearCompleted: PropTypes.func,
+};
--- src/components/Footer/TasksFilter/TasksFilter.jsx
+++ src/components/Footer/TasksFilter/TasksFilter.jsx
@@ -1,53 +1,44 @@
-import { Component } from 'react';
 import './TasksFilter.css';
 import PropTypes from 'prop-types';
 
-export default class TasksFilter extends Component {
-  static propTypes = {
-    filter: PropTypes.string,
-    selectTasks: PropTypes.func,
-  };
+export default function TasksFilter({ filter, selectTasks }) {
+  let allClass = '';
+  let activeClass = '';
+  let completedClass = '';
 
-  static defaultProps = {
-    filter: 'all',
-  };
-
-  render() {
-    let allClass = '';
-    let activeClass = '';
-    let completedClass = '';
-
-    const { filter } = this.props;
-
-    switch (filter) {
-      case 'all':
-        allClass += 'selected';
-        break;
-      case 'active':
-        activeClass += 'selected';
-        break;
-      case 'completed':
-        completedClass += 'selected';
-    }
-
-    return (
-      <ul className="filters">
-        <li>
-          <button className={allClass} onClick={() => this.props.selectTasks('all')}>
-            All
-          </button>
-        </li>
-        <li>
-          <button className={activeClass} onClick={() => this.props.selectTasks('active')}>
-            Active
-          </button>
-        </li>
-        <li>
-          <button className={completedClass} onClick={() => this.props.selectTasks('completed')}>
-            Completed
-          </button>
-        </li>
-      </ul>
-    );
+  switch (filter) {
+    case 'all':
+      allClass += 'selected';
+      break;
+    case 'active':
+      activeClass += 'selected';
+      break;
+    case 'completed':
+      completedClass += 'selected';
   }
+
+  return (
+    <ul className="filters">
+      <li>
+        <button className={allClass} onClick={() => selectTasks('all')}>
+          All
+        </button>
+      </li>
+      <li>
+        <button className={activeClass} onClick={() => selectTasks('active')}>
+          Active
+        </button>
+      </li>
+      <li>
+        <button className={completedClass} onClick={() => selectTasks('completed')}>
+          Completed
+        </button>
+      </li>
+    </ul>
+  );
 }
+
+TasksFilter.propTypes = {
+  filter: PropTypes.string,
+  selectTasks: PropTypes.func,
+};
