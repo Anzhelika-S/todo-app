ce5f73044c2bf4a5e403b0208d46b028143b4705
Anzhelika <anzhelikasilkina@yandex.ru>
2025-02-08 23:15:26 +0300

    rewrite newtaskform with hooks

--- src/components/NewTaskForm/NewTaskForm.jsx
+++ src/components/NewTaskForm/NewTaskForm.jsx
@@ -1,100 +1,89 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './NewTaskForm.css';
 import PropTypes from 'prop-types';
 
-export default class NewTaskForm extends Component {
-  static propTypes = {
-    handleKey: PropTypes.func,
+export default function NewTaskForm({ handleKey }) {
+  const [value, setValue] = useState('');
+  const [min, setMin] = useState('');
+  const [sec, setSec] = useState('');
+  const [time, setTime] = useState('');
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
   };
 
-  state = {
-    value: '',
-    min: '',
-    sec: '',
-    time: '',
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      value: e.target.value,
-    });
-  };
-
-  onMinuteChange = (e) => {
+  const onMinuteChange = (e) => {
     const value = e.target.value;
     if (/^\d*$/.test(value)) {
       const number = Number(value);
       if (number >= 0 && number <= 59) {
-        this.setState({ min: number, time: number * 60 });
+        setMin(number);
+        setTime(number * 60);
       } else if (value === '') {
         return;
       }
     }
   };
 
-  onSecChange = (e) => {
+  const onSecChange = (e) => {
     const value = e.target.value;
     if (/^\d*$/.test(value)) {
       const number = Number(value);
       if (number >= 0 && number <= 59) {
-        this.setState((state) => ({
-          time: (state.min || 0) * 60 + number,
-          sec: number,
-        }));
+        setSec(number);
+        setTime((min || 0) * 60 + number);
       } else if (value === '') {
         return;
       }
     }
   };
 
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    const { time, value } = this.state;
 
-    value && time >= 0 ? this.props.handleKey(value, time) : 0;
+    value && time >= 0 ? handleKey(value, time) : 0;
 
-    this.setState({
-      value: '',
-      min: '',
-      sec: '',
-      time: '',
-    });
+    setValue('');
+    setSec('');
+    setMin('');
+    setTime('');
   };
 
-  onKeyDown = (e) => {
+  const onKeyDown = (e) => {
     if (e.key === 'Enter') {
-      this.onSubmit(e);
+      onSubmit(e);
     }
   };
 
-  render() {
-    const { sec, min, value } = this.state;
-    return (
-      <form onSubmit={this.onSubmit} onKeyDown={this.onKeyDown} className="new-todo-form">
-        <input
-          className="new-todo"
-          placeholder="What needs to be done?"
-          autoFocus
-          onChange={this.handleChange}
-          value={value}
-        />
-        <input
-          className="new-todo new-todo-form__timer"
-          placeholder="Min"
-          onInput={this.onMinuteChange}
-          maxLength={2}
-          pattern="\d*"
-          value={min}
-        />
-        <input
-          className="new-todo new-todo-form__timer"
-          placeholder="Sec"
-          onInput={this.onSecChange}
-          maxLength={2}
-          pattern="\d*"
-          value={sec}
-        />
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={onSubmit} onKeyDown={onKeyDown} className="new-todo-form">
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        autoFocus
+        onChange={handleChange}
+        value={value}
+      />
+      <input
+        className="new-todo new-todo-form__timer"
+        placeholder="Min"
+        onInput={onMinuteChange}
+        maxLength={2}
+        pattern="\d*"
+        value={min}
+      />
+      <input
+        className="new-todo new-todo-form__timer"
+        placeholder="Sec"
+        onInput={onSecChange}
+        maxLength={2}
+        pattern="\d*"
+        value={sec}
+      />
+    </form>
+  );
 }
+
+NewTaskForm.propTypes = {
+  handleKey: PropTypes.func,
+};
