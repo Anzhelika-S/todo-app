3a7a7987996a3b32c010ab1d6202bda36dc02c11
Anzhelika <anzhelikasilkina@yandex.ru>
2025-02-08 22:51:56 +0300

    rewrite tasklist with hooks

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,41 +1,36 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Task from '../Task';
 import './TaskList.css';
 
-export default class TaskList extends Component {
-  static propTypes = {
-    onToggleCompleted: PropTypes.func,
-    onToggleEditing: PropTypes.func,
-    onDeleted: PropTypes.func,
-    onEdit: PropTypes.func,
-    tasks: PropTypes.array,
-  };
-
-  render() {
-    const { tasks, onToggleCompleted, onToggleEditing, onDeleted, onEdit, handleTimer } = this.props;
-
-    const elements = tasks.map((item) => {
-      return (
-        <Task
-          key={item.id}
-          id={item.id}
-          onDeleted={onDeleted}
-          value={item.value}
-          sec={item.sec}
-          completed={item.completed}
-          editing={item.editing}
-          onToggleCompleted={() => onToggleCompleted(item.id)}
-          onToggleEditing={() => onToggleEditing(item.id)}
-          onEdit={onEdit}
-          handleTimer={handleTimer}
-          createdAt={item.createdAt}
-          intervalID={item.intervalID}
-          running={item.running}
-        />
-      );
-    });
-    return <ul className="list-group todo-list">{elements}</ul>;
-  }
+export default function TaskList({ tasks, onToggleCompleted, onToggleEditing, onDeleted, onEdit, handleTimer }) {
+  const elements = tasks.map((item) => {
+    return (
+      <Task
+        key={item.id}
+        id={item.id}
+        onDeleted={onDeleted}
+        value={item.value}
+        sec={item.sec}
+        completed={item.completed}
+        editing={item.editing}
+        onToggleCompleted={() => onToggleCompleted(item.id)}
+        onToggleEditing={() => onToggleEditing(item.id)}
+        onEdit={onEdit}
+        handleTimer={handleTimer}
+        createdAt={item.createdAt}
+        intervalID={item.intervalID}
+        running={item.running}
+      />
+    );
+  });
+  return <ul className="list-group todo-list">{elements}</ul>;
 }
+
+TaskList.propTypes = {
+  onToggleCompleted: PropTypes.func,
+  onToggleEditing: PropTypes.func,
+  onDeleted: PropTypes.func,
+  onEdit: PropTypes.func,
+  tasks: PropTypes.array,
+};
