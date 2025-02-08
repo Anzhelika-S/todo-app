a49dab0347c3848a02608e8916b796987a56ec66
Anzhelika <anzhelikasilkina@yandex.ru>
2025-02-08 23:17:32 +0300

    rewrite header as function component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,17 @@
 import './Header.css';
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import NewTaskForm from '../NewTaskForm';
 
-export default class Header extends Component {
-  static propTypes = {
-    handleKey: PropTypes.func,
-  };
-
-  render() {
-    return (
-      <header>
-        <h1 className="header">todos</h1>
-        <NewTaskForm handleKey={this.props.handleKey} />
-      </header>
-    );
-  }
+export default function Header({ handleKey }) {
+  return (
+    <header>
+      <h1 className="header">todos</h1>
+      <NewTaskForm handleKey={handleKey} />
+    </header>
+  );
 }
+
+Header.propTypes = {
+  handleKey: PropTypes.func,
+};
